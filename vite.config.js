2b0266bceef4bb58f820fcb12f3f06c5282544f8
Gandalf <lynxdev@protonmail.com>
2024-01-17 16:53:40 +0200

    add: deploy workflow 4

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/ticketcrew-prototype/"
+  base: "https://lynixiox.github.io/ticketcrew-prototype/"
 })
