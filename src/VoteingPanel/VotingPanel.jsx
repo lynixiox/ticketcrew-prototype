fdadb1f00f9932f2ca4452b3215c1716e3e50b24
Gandalf <lynxdev@protonmail.com>
2024-01-18 13:01:12 +0200

    added confirmation

--- src/VoteingPanel/VotingPanel.jsx
+++ src/VoteingPanel/VotingPanel.jsx
@@ -18,6 +18,10 @@ const VotingPanel = () => {
     const [selectedOption, setSelectedOption] = useState("")
     const [amount, setAmount] = useState("0")
     const [count, setCount] = useState("");
+    const [isCheckout, setCheckout] = useState(false);
+    const [Name, setName] = useState("");
+    const [Surname, setSurname] = useState("");
+    const [email, setEmail] = useState("");
 
 
     return(
@@ -28,18 +32,40 @@ const VotingPanel = () => {
                         Vote Now!
                     </styled.headingContainer>
                     <styled.componentContainer>
+                        {isCheckout ? 
+
+                           <>
+                                <styled.componentItem>
+                                    <TextInput label="Name" textColor="#fff" borderBottomColor="#fff" value={Name} onChange={(event) => {setName(event.target.value)}}/>
+                                </styled.componentItem>
+                                <styled.componentItem>
+                                    <TextInput label="Surname" textColor="#fff" borderBottomColor="#fff" value={Surname} onChange={(event) => {setSurname(event.target.value)}}/>
+                                </styled.componentItem>
+                                <styled.componentItem>
+                                    <TextInput label="Email for Confirmation" textColor="#fff" borderBottomColor="#fff" value={email} onChange={(event) => {setEmail(event.target.value);}}/>
+                                </styled.componentItem>
+                                <styled.componentItem>
+                       <CustomButton label="Confirm" width="150px" onClick={()=> {setCheckout(true)}}/>
+                   </styled.componentItem>
+                           </>
+                        :
+                        
+                        <>
                         <styled.componentItem>
-                            <ComboBox key="bundle" label="Select Bundle" width="50%" textColor="#fff" borderBottomColor="#fff" optionList={optionList} value={selectedOption} onChange={(event)=> {setSelectedOption(event.target.value); }}/>
-                        </styled.componentItem>
-                        <styled.componentItem>
-                            <TextInput label="Quantity" textColor="#fff" borderBottomColor="#fff" value={count} onChange={(event) => {setCount(event.target.value); setAmount(selectedOption * event.target.value)}}/>
-                        </styled.componentItem>
-                        <styled.componentItem>
-                            <LabelPlain value={"Total: R" + amount}/>
-                        </styled.componentItem>
-                        <styled.componentItem>
-                            <CustomButton label="Checkout" width="150px"/>
-                        </styled.componentItem>
+                       <ComboBox key="bundle" label="Select Bundle" width="50%" textColor="#fff" borderBottomColor="#fff" optionList={optionList} value={selectedOption} onChange={(event)=> {setSelectedOption(event.target.value); }}/>
+                   </styled.componentItem>
+                   <styled.componentItem>
+                       <TextInput label="Quantity" textColor="#fff" borderBottomColor="#fff" value={count} onChange={(event) => {setCount(event.target.value); setAmount(selectedOption * event.target.value)}}/>
+                   </styled.componentItem>
+                   <styled.componentItem>
+                       <LabelPlain value={"Total: R" + amount}/>
+                   </styled.componentItem>
+                   <styled.componentItem>
+                       <CustomButton label="Checkout" width="150px" onClick={()=> {setCheckout(true)}}/>
+                   </styled.componentItem>
+</>
+                        }
+
                     </styled.componentContainer>
                 </styled.ContentContainer>
             </styled.VotingPanelContainer>
