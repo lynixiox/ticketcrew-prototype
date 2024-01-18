7d17d7e5c16a850f5f23ac1d6c05ca8af9152b48
Gandalf <lynxdev@protonmail.com>
2024-01-18 12:44:52 +0200

    added total payment times quantity

--- src/VoteingPanel/VotingPanel.jsx
+++ src/VoteingPanel/VotingPanel.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import * as styled from './VotingPanel.styles'
 import ComboBox from "../combobox/ComboBox";
 import TextInput from "../textInput/TextInput";
@@ -9,15 +9,17 @@ import LabelPlain from "../label/LabelPlain";
 const VotingPanel = () => {
 
     const optionList = [
-        {optionValue: "R10", optionName: "5 VOTE BUNDLE (R2 p/vote)"},
-        {optionValue: "R180", optionName: "100 VOTE BUNDLE (R1.80 p/vote)"},
-        {optionValue: "R250", optionName: "200 VOTE BUNDLE (R1.50 p/vote)"},
-        {optionValue: "R500", optionName: "500 VOTE BUNDLE (R1 p/vote)"},
+        {optionValue: "10", optionName: "5 VOTE BUNDLE (R2 p/vote)"},
+        {optionValue: "180", optionName: "100 VOTE BUNDLE (R1.80 p/vote)"},
+        {optionValue: "250", optionName: "200 VOTE BUNDLE (R1.50 p/vote)"},
+        {optionValue: "500", optionName: "500 VOTE BUNDLE (R1 p/vote)"},
     ]
 
     const [selectedOption, setSelectedOption] = useState("")
     const [amount, setAmount] = useState("0")
-    const [count, setCount] = useState(1);
+    const [count, setCount] = useState("");
+
+
     return(
         <>
             <styled.VotingPanelContainer>
@@ -27,13 +29,13 @@ const VotingPanel = () => {
                     </styled.headingContainer>
                     <styled.componentContainer>
                         <styled.componentItem>
-                            <ComboBox key="bundle" label="Select Bundle" width="50%" textColor="#fff" borderBottomColor="#fff" optionList={optionList} value={selectedOption} onChange={(event)=> {setSelectedOption(event.target.value); setAmount(event.target.value)}}/>
+                            <ComboBox key="bundle" label="Select Bundle" width="50%" textColor="#fff" borderBottomColor="#fff" optionList={optionList} value={selectedOption} onChange={(event)=> {setSelectedOption(event.target.value); }}/>
                         </styled.componentItem>
                         <styled.componentItem>
-                            <TextInput label="Quantity" textColor="#fff" borderBottomColor="#fff"/>
+                            <TextInput label="Quantity" textColor="#fff" borderBottomColor="#fff" value={count} onChange={(event) => {setCount(event.target.value); setAmount(selectedOption * event.target.value)}}/>
                         </styled.componentItem>
                         <styled.componentItem>
-                            <LabelPlain value={"Total: " + amount}/>
+                            <LabelPlain value={"Total: R" + amount}/>
                         </styled.componentItem>
                         <styled.componentItem>
                             <CustomButton label="Checkout" width="150px"/>
